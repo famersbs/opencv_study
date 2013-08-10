49e6e98911d458151badba3ff5eae59e4865dd4b
서병선 <famersbs@song-ui-MacBook-Air.local>
2013-08-10 14:10:54 +0900

    * 꼼수

---
+++ tesseract_firsttest/src/tesseract_firsttest.cpp
@@ -0,0 +1,47 @@
+//============================================================================
+// Name        : tesseract_firsttest.cpp
+// Author      : famersbs
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+
+#include "leptonica/allheaders.h"
+#include "tesseract/baseapi.h"
+
+int main() {
+
+	// [1]
+	tesseract::TessBaseAPI *myOCR = new tesseract::TessBaseAPI();
+
+	// [2]
+	printf("Tesseract-ocr version: %s\n",
+		   myOCR->Version());
+	printf("Leptonica version: %s\n",
+		   getLeptonicaVersion());
+
+	// [3]
+	if (myOCR->Init( 0 , "eng+kor")) {		// 동시에 처리는 가능하지만... 엄청 느리내 ~!
+	  fprintf(stderr, "Could not initialize tesseract.\n");
+	  exit(1);
+	}
+
+	// [4]
+	Pix *pix = pixRead("./res/input.png");
+	myOCR->SetImage(pix);
+
+	// [5]
+	char* outText = myOCR->GetUTF8Text();
+	printf("OCR output:\n\n");
+	printf("%s", outText);
+
+	// [6]
+	myOCR->Clear();
+	myOCR->End();
+	delete [] outText;
+	pixDestroy(&pix);
+
+	return 0;
+}
