53d96ece47e835ded039776e43243dde310ce9d4
famersbs <famersbs@gmail.com>
2013-08-21 18:06:48 +0900

    - 추가 작업 수행

---
+++ webplatform_test/WebContent/angularjs/pptapp/js/app.js
@@ -0,0 +1,10 @@
+angular.module('pptApp', []).
+  config(['$routeProvider', function($routeProvider) {
+	  
+	  /*
+  $routeProvider.
+      when('/phones', {templateUrl: 'phone-list.html',   controller: PhoneListCtrl}).
+      when('/phones/:phoneId', {templateUrl: 'phone-detail.html', controller: PhoneDetailCtrl}).
+      otherwise({redirectTo: '/phones'});
+      */
+}]);
