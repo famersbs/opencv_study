3b1d4c94fd77df8b181c6d58be2707382e8b7073
famersbs <famersbs@gmail.com>
2014-01-28 15:38:30 +0900

    famersbs example commit

---
+++ learningOpencv-bookstudy/example/famersbs/src/example5/example5.h
@@ -0,0 +1,16 @@
+/*
+ * example5.h
+ *
+ *  Created on: 2013. 12. 14.
+ *      Author: famersbs
+ */
+
+#ifndef EXAMPLE5_H_
+#define EXAMPLE5_H_
+
+
+
+void run_example5();
+
+
+#endif /* EXAMPLE5_H_ */
